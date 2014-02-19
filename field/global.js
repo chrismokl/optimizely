0f37ad01c56f4eab0e1c5eef2cd236d0135d28b0
Chris Mo <cmo871@gmail.com>
2014-02-18 16:14:27 -0800

    delete readme

--- field/global.js
+++ field/global.js
@@ -1,23 +1,5 @@
 /*
 $('#modalSignup-join-a form').submit(function(){
-	console.log('join a submit success');
-	var $field = $('#modalSignup-join-b p');
-	var $buttonb = $('#modalSignup-join-b button:eq(0)')
-	$('#signupModal').hide();
-	$field.find(':eq(0)').attr('value','email@email.com');
-	$field.find(':eq(1)').attr('value','test');
-	$field.find(':eq(2)').attr('value','asdfasfd123456');
-	$buttonb.click();
-});
-
-var email = $('#modalSignup-join-a #email').val().split('@')
-var name = email[0];
-var suffix = '@' + email[1];
-
-
-
-
-$('#modalSignup-join-a form').submit(function(){
 	$('#modalSignup-join-a form').ajaxSuccess(function(){
 	console.log('got into first ajaxsuccess');
 	  $('#signupModal').hide();
